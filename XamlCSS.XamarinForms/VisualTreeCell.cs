05a5460f052658f7c7d6eaba758c57492664728c
David Rettenbacher <spoiltnr1@gmx.at>
2017-01-21 03:20:30 +0100

    Closes warappa/XamlCSS/issues/13 Xamarin.Forms: Style disappears when resuming Android app (2nd try)

--- XamlCSS.XamarinForms/VisualTreeCell.cs
+++ XamlCSS.XamarinForms/VisualTreeCell.cs
@@ -4,70 +4,71 @@ using XamlCSS.Windows.Media;
 
 namespace XamlCSS.XamarinForms
 {
-	public static class VisualTreeCell
-	{
-		public static readonly BindableProperty IncludeProperty =
-			BindableProperty.CreateAttached(
-				"Include",
-				typeof(bool),
-				typeof(VisualTreeCell),
-				false,
-				propertyChanged: OnIncludeChanged);
+    public static class VisualTreeCell
+    {
+        public static readonly BindableProperty IncludeProperty =
+            BindableProperty.CreateAttached(
+                "Include",
+                typeof(bool),
+                typeof(VisualTreeCell),
+                false,
+                propertyChanged: OnIncludeChanged);
 
-		public static bool GetInclude(BindableObject view)
-		{
-			return (bool)view.GetValue(IncludeProperty);
-		}
+        public static bool GetInclude(BindableObject view)
+        {
+            return (bool)view.GetValue(IncludeProperty);
+        }
 
-		public static void SetInclude(BindableObject view, bool value)
-		{
-			view.SetValue(IncludeProperty, value);
-		}
+        public static void SetInclude(BindableObject view, bool value)
+        {
+            view.SetValue(IncludeProperty, value);
+        }
 
-		static void OnIncludeChanged(BindableObject view, object oldValue, object newValue)
-		{
-			var entry = view as Cell;
-			if (entry == null)
-			{
-				return;
-			}
+        static void OnIncludeChanged(BindableObject view, object oldValue, object newValue)
+        {
+            var entry = view as Cell;
+            if (entry == null)
+            {
+                return;
+            }
 
-			bool register = (bool)newValue;
-			if (register)
-			{
-				entry.Appearing += Entry_Appearing;
-				entry.Disappearing += Entry_Disappearing;
-				entry.PropertyChanged += Entry_PropertyChanged;
-			}
-			else
-			{
-				entry.Appearing -= Entry_Appearing;
-				entry.Disappearing -= Entry_Disappearing;
-				entry.PropertyChanged -= Entry_PropertyChanged;
-			}
-		}
+            bool register = (bool)newValue;
+            if (register)
+            {
+                entry.Appearing += Entry_Appearing;
+                entry.PropertyChanged += Entry_PropertyChanged;
+                entry.BindingContextChanged += Entry_BindingContextChanged;
+            }
+            else
+            {
+                entry.Appearing -= Entry_Appearing;
+                entry.PropertyChanged -= Entry_PropertyChanged;
+                entry.BindingContextChanged -= Entry_BindingContextChanged;
+            }
+        }
 
-		private static void Entry_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == "Parent")
-			{
-				var s = sender as Element;
-				if (s.Parent != null)
-					VisualTreeHelper.Include(sender as Element);
-				else
-					VisualTreeHelper.Exclude(sender as Element);
-			}
-		}
+        private static void Entry_BindingContextChanged(object sender, EventArgs e)
+        {
+            VisualTreeHelper.Exclude(sender as Element);
+            VisualTreeHelper.Include(sender as Element);
+        }
 
-		private static void Entry_Disappearing(object sender, EventArgs e)
-		{
-			//Debug.WriteLine("Entry_Disappearing");
-			VisualTreeHelper.Exclude(sender as Element);
-		}
 
-		private static void Entry_Appearing(object sender, EventArgs e)
-		{
-			VisualTreeHelper.Include(sender as Element);
-		}
-	}
+        private static void Entry_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Parent")
+            {
+                var s = sender as Element;
+                if (s.Parent != null)
+                    VisualTreeHelper.Include(sender as Element);
+                else
+                    VisualTreeHelper.Exclude(sender as Element);
+            }
+        }
+
+        private static void Entry_Appearing(object sender, EventArgs e)
+        {
+            VisualTreeHelper.Include(sender as Element);
+        }
+    }
 }
