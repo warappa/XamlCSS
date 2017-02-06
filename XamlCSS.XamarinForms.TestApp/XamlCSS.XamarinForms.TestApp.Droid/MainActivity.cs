363d7d8829a556e10a2079c67e2f73f98f314610
David Rettenbacher <spoiltnr1@gmx.at>
2017-02-06 22:26:32 +0100

    Android: Sample app: Prevent reloading app after pressing back-button and coming back again

--- XamlCSS.XamarinForms.TestApp/XamlCSS.XamarinForms.TestApp.Droid/MainActivity.cs
+++ XamlCSS.XamarinForms.TestApp/XamlCSS.XamarinForms.TestApp.Droid/MainActivity.cs
@@ -4,16 +4,21 @@ using Android.OS;
 
 namespace XamlCSS.XamarinForms.TestApp.Droid
 {
-	[Activity(Label = "XamlCSS.XamarinForms.TestApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
+    [Activity(Label = "XamlCSS.XamarinForms.TestApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init(this, bundle);
-			LoadApplication(new App());
-		}
-	}
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnBackPressed()
+        {
+            MoveTaskToBack(true);
+        }
+    }
 }
 
