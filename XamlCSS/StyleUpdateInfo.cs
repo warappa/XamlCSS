4aa708aa078d543762cc137401135b7a80c64de7
David Rettenbacher <spoiltnr1@gmx.at>
2018-01-17 00:30:00 +0100

    Merged BaseCss2 into BaseCss

---
+++ XamlCSS/StyleUpdateInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XamlCSS
+{
+    public class StyleUpdateInfo
+    {
+        public StyleSheet OldStyleSheet { get; set; }
+        public List<string> OldMatchedSelectors { get; set; }
+        public StyleSheet CurrentStyleSheet { get; set; }
+        public List<string> CurrentMatchedSelectors { get; set; } = new List<string>();
+        public Type MatchedType { get; internal set; }
+        public SelectorType DoMatchCheck { get; internal set; }
+    }
+}
