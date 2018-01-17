50997ccffaed6045e3d7507b76b1aa81edefbbea
David Rettenbacher <spoiltnr1@gmx.at>
2018-01-17 21:17:55 +0100

    missed updated file

--- XamlCSS/IDependencyPropertyService.cs
+++ XamlCSS/IDependencyPropertyService.cs
@@ -12,26 +12,11 @@ namespace XamlCSS
 
         void RegisterLoadedOnce(TDependencyObject frameworkElement, Action<object> func);
 
-        string[] GetMatchingStyles(TDependencyObject obj);
-        void SetMatchingStyles(TDependencyObject obj, string[] value);
-
-        string[] GetAppliedMatchingStyles(TDependencyObject obj);
-        void SetAppliedMatchingStyles(TDependencyObject obj, string[] value);
-
-        bool GetHandledCss(TDependencyObject obj);
-        void SetHandledCss(TDependencyObject obj, bool value);
-
-        StyleSheet GetStyledByStyleSheet(TDependencyObject obj);
-        void SetStyledByStyleSheet(TDependencyObject obj, StyleSheet value);
-
         string GetName(TDependencyObject obj);
         void SetName(TDependencyObject obj, string value);
 
         TStyle GetInitialStyle(TDependencyObject obj);
         void SetInitialStyle(TDependencyObject obj, TStyle value);
-
-        bool? GetHadStyle(TDependencyObject obj);
-        void SetHadStyle(TDependencyObject obj, bool? value);
 
         StyleDeclarationBlock GetStyle(TDependencyObject obj);
         void SetStyle(TDependencyObject obj, StyleDeclarationBlock value);
