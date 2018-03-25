e53516c520647edfb3a6cc6e5d100cd3069ceef2
David Rettenbacher <spoiltnr1@gmx.at>
2018-03-25 11:48:14 +0200

    Version 2.2.0-pre1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyCopyright("© 2016-2017 David Rettenbacher")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
